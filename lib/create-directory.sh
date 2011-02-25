b3b7eef9c11123a2e1b79c79084402c11620341a
Anthony Williams <hi@antw.me>
2011-02-25 18:31:15 +0000

    Don't need to quote directory arguments.

--- lib/create-directory.sh
+++ lib/create-directory.sh
@@ -5,7 +5,8 @@
 # $3 -> user:group passed to chown.
 #
 genesis_create_dir() {
-  mkdir -p "$($1)"
-  chmod $2 "$($1)"
-  chown "$($3)" "$($1)"
+  genesis_say_with_time "Creating directory $1"
+  mkdir -p $1
+  chmod $2 $1
+  chown $3 $1
 }
