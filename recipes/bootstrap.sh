3334be444e72adf3a996624d633d60a913f47d8b
Anthony Williams <hi@antw.me>
2011-02-25 18:02:08 +0000

    Clean up bootstrap.

--- recipes/bootstrap.sh
+++ recipes/bootstrap.sh
@@ -1,11 +1,4 @@
 # This recipe should be run on all new servers in order to perform
 # general setup tasks.
 
-echo -e "\n"
-echo "=> Beginning bootstrap."
-
 sudo apt-get update
-
-echo -e "\n"
-echo "=> Bootstrap complete."
-echo -e "\n"
