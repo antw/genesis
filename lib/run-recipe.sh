5b687ac962cef28b6a9d1c66676fe664d177fba7
Anthony Williams <hi@antw.me>
2011-02-25 18:31:42 +0000

    Adjust verbosity!

--- lib/run-recipe.sh
+++ lib/run-recipe.sh
@@ -2,9 +2,8 @@ genesis_run_recipe() {
   if [[ "$1" = "" ]] ; then
     echo "Ran genesis_run_recipe with no recipe name"
   elif [ -f "./recipes/$1.sh" ] ; then
-    genesis_say_with_time "Starting recipe: $1"
+    genesis_say_header "Running recipe: $1"
     source "./recipes/$1.sh"
-    genesis_say_with_time "Finished recipe: $1"
   else
     echo "No such recipes: $1"
     exit 1
--- lib/run-role.sh
+++ lib/run-role.sh
@@ -2,9 +2,7 @@ genesis_run_role() {
   if [[ "$1" = "" ]] ; then
     echo "Ran genesis_run_role with no role name"
   elif [ -f "./roles/$1.sh" ] ; then
-    genesis_say_with_time "Starting role: $1"
     source "./roles/$1.sh"
-    genesis_say_with_time "Finished role: $1"
   else
     echo "No such role: $1"
     exit 1
