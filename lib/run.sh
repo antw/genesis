b87633114ca3bcd1c2be287f1ee4be314fc21d1e
Anthony Williams <hi@antw.me>
2011-02-25 23:09:02 +0000

    Organise recipies, roles, and servers into their own directories.

--- lib/run.sh
+++ lib/run.sh
@@ -1,32 +1,42 @@
 # Runs a single recipe. Expected to be at $genesis_path/recipes/$1.sh
 #
 genesis_run_recipe() {
-  if [[ "$1" = "" ]] ; then
-    echo "Ran genesis_run_recipe with no recipe name"
-
-  elif [ -f "$genesis_path/recipes/$1.sh" ] ; then
-    genesis_say_header "Running recipe: $1"
-    source "$genesis_path/recipes/$1.sh"
-
-  else
-    echo "No such recipe: $1"
-    exit 1
-
-  fi
+  [[ ! "$1" = "" ]] && genesis_say_header "Running recipe: $1"
+  __genesis_run_component "recipe" $1
 }
 
 # Runs a single role. Expected to be at $genesis_path/roles/$1.sh
 #
 genesis_run_role() {
-  if [[ "$1" = "" ]] ; then
-    echo "Ran genesis_run_role with no role name"
+  __genesis_run_component "role" $1
+}
 
-  elif [ -f "$genesis_path/roles/$1.sh" ] ; then
-    source "$genesis_path/roles/$1.sh"
+# Runs something; a recipe, role, or server.
+#
+# $1 = A noun: "recipe", "role", or "server".
+# $2 = A thing: e.g. "rails", "app-server", etc.
+#
+__genesis_run_component() {
+
+  local component_dir="$genesis_path/$1s/$2"
+  # naive pluralisation - - - - - - - ^
+
+  if [[ "$2" = "" ]] ; then
+    echo "Ran genesis_run_$1 with no $1 provided"
+
+  elif [ -f "$component_dir/run.sh" ] ; then
+    source "$component_dir/run.sh"
+
+    # If the recipe has any fixture files, copy them.
+    [ -d "$component_dir/files" ] && copy -r "$component_dir/files" /
+
+    # If the recipe has an after "filter", run it now.
+    [ -f "$component_dir/after.sh" ] && source "$component_dir/after.sh"
 
   else
-    echo "No such role: $1"
+    echo "No such $1: $2"
     exit 1
 
   fi
+
 }
