7d07a10ab33fea70dbe473be2c0d955014d78cd1
Anthony Williams <hi@antw.me>
2011-02-25 18:26:47 +0000

    Spelling fix.

--- lib/run-recipe.sh
+++ lib/run-recipe.sh
@@ -4,7 +4,7 @@ genesis_run_recipe() {
   elif [ -f "./recipes/$1.sh" ] ; then
     genesis_say_with_time "Starting recipe: $1"
     source "./recipes/$1.sh"
-    genesis_say_with_time "Finished reipe: $1"
+    genesis_say_with_time "Finished recipe: $1"
   else
     echo "No such recipes: $1"
     exit 1
