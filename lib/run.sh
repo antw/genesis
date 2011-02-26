b46ab5f1c55ee18338b91c519561cc077d5fd61a
Anthony Williams <hi@antw.me>
2011-02-26 16:58:21 +0000

    Correct path expansion when copying files.

--- lib/run.sh
+++ lib/run.sh
@@ -38,7 +38,7 @@ __genesis_run_component() {
     [ -f "$component_dir/run.sh" ] && source "$component_dir/run.sh"
 
     # If the recipe has any fixture files, copy them.
-    [ -d "$component_dir/files" ] && copy -r "$component_dir/files" /
+    [ -d "$component_dir/files" ] && cp -r $component_dir/files/* /
 
     # If the recipe has an after "filter", run it now.
     [ -f "$component_dir/after.sh" ] && source "$component_dir/after.sh"
