fa22e358f4b17d2b5a0b0c9991d5904b8a99bb3f
Anthony Williams <hi@antw.me>
2011-02-28 20:38:48 +0000

    Keep track of recipes which have already been run.

--- lib/components.sh
+++ lib/components.sh
@@ -1,21 +1,34 @@
+# Keeps track of which recipes have been run, so that it is safe to call
+# run_recipe multiple times with the same recipe, without having to
+# worry about repeating work.
+declare -a run_recipes
+run_recipes=( '' )
+
 # Runs a single recipe. Expected to be at $path/recipes/$1.sh
 #
 run_recipe() {
-  [[ ! "$1" = "" ]] && say_header "Running recipe: $1"
-  run_component "recipe" $1
+  local recipe_name=$1
+
+  if [ $( array_contains $recipe_name ${run_recipes[@]} ) = -1 ] ; then
+    say_header "Running recipe: $1"
+    __run_component "recipe" $1
+    array_push 'run_recipes' $recipe_name
+  fi
+
+  return 0
 }
 
 # Runs a single role. Expected to be at $path/roles/$1.sh
 #
 run_role() {
-  run_component "role" $1
+  __run_component "role" $1
 }
 
 # Runs a single server declaration. Expected to be at
 # $path/servers/$1/run.sh
 #
 run_server() {
-  run_component "server" $1
+  __run_component "server" $1
 }
 
 # Runs something; a recipe, role, or server.
@@ -23,7 +36,7 @@ run_server() {
 # $1 = A noun: "recipe", "role", or "server".
 # $2 = A thing: e.g. "rails", "app-server", etc.
 #
-run_component() {
+__run_component() {
 
   local component_path="$genesis_path/$1s/$2.sh"
   # naive pluralisation - - - - - - - ^
