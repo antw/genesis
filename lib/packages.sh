c79659e68e884786de08a51f26afa9bae1554523
Anthony Williams <hi@antw.me>
2011-02-28 20:35:58 +0000

    Clean up packages.sh

--- lib/packages.sh
+++ lib/packages.sh
@@ -1,22 +1,20 @@
 # Contains functions for helping manage package and recipe dependencies
 # so that each one is run at most once.
 
-declare -a package_deps
-declare -a recipe_deps
+declare -a installed_packages
 
 # For some reason, these need an empty element or Bash complains that
 # the variables aren't set.
-package_deps=( '' )
-recipe_deps=( '' )
+installed_packages=( '' )
 
 # Package which needs to be installed.
 install_package() {
   local package_name=$1
 
-  if [ $( array_contains $package_name ${package_deps[@]} ) = -1 ] ; then
+  if [ $( array_contains $package_name ${installed_packages[@]} ) = -1 ] ; then
     say_with_time "Installing package: $package_name"
     aptitude install -y $package_name
-    array_push 'package_deps' $package_name
+    array_push 'installed_packages' $package_name
   fi
 
   return 0
