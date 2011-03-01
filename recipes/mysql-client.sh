459a5b9e9caf122f5287b343d5c835bc939e475a
Anthony Williams <hi@antw.me>
2011-03-01 17:43:05 +0000

    Add mysql-client recipe.

---
+++ recipes/mysql-client.sh
@@ -0,0 +1,6 @@
+# Installs the MySQL client libraries. Suitable for application and
+# search servers where there is no need to run the MySQL server.
+#
+
+install_package 'libmysqlclient-dev'
+install_package 'mysql-client'
